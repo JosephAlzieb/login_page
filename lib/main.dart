1cd6c3757c9b705008a04603f0e5ad619a726bf6
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-27 11:37:14 +0100

    make the app start with the login-page

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:login_page/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPgae()
+    );
+  }
+}
